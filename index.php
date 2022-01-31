28efbdac06d0c42700815d7ca3c1d9b4494fad02
YurekhaAdluru <98517983+YurekhaAdluru@users.noreply.github.com>
2022-01-31 10:29:36 +0000

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 	</head>
 
 <body> 
-	<h1>PHP 2022  Test</h1>
+	<h1>Yurekha  Test</h1>
 		<p><b> CITIBANK Session </b></p>
 		<?php echo "The Current Date and yes las  Time is: <br />";
 		echo date("g:i A l, F j Y.");?> </p>
